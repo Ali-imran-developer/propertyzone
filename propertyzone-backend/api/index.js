51bba469dbf6522e1e19aedbf846f0b660290676
Ali-imran-developer <ai8440018@gmail.com>
2025-07-24 12:28:44 +0500

    upload project

---
+++ propertyzone-backend/api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./dbConnect");
+const serverless = require("serverless-http");
+require("dotenv").config();
+
+const app = express();
+dbConnect();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", authRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
