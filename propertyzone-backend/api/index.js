6616da8f8395732f85899fe7db337f37c1fa29f1
Ali-imran-developer <ai8440018@gmail.com>
2025-08-01 19:39:31 +0500

    add more buffering time to api

--- propertyzone-backend/api/index.js
+++ propertyzone-backend/api/index.js
@@ -8,15 +8,15 @@ require("dotenv").config();
 const app = express();
 dbConnect();
 app.use(cors());
-app.use(express.json());
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use("/api/property", propertyRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);
